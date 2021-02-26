7ee02c44fafd4dec9ceb154cf5b25d34882723b4
ajithvallabai <inocajith21.5@gmail.com>
2021-02-26 16:18:14 +0530

    bug fix

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import numpy as np 
-from flask import Flask, request, jsonify, render_template 
-import pickle 
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -11,18 +11,17 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    """
-    input is got from user and prediction is done
-    """
+    '''
+    For rendering results on HTML GUI
+    '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+    return render_template('index.html', prediction_text='Employee Salary should be Rs. {}'.format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
